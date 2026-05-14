ff5636738d7b71e5989d79849b7e54927d6f4b84
David Neumann <davidjneumann@gmail.com>
2026-05-14 09:30:19 -0400

    Sync clibpoard with remote host

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -40,3 +40,17 @@ vim.opt.undofile = true
 vim.opt.updatetime = 250
 vim.opt.wrap = true
 vim.o.signcolumn = 'yes'
+
+if vim.env.SSH_TTY then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+end
