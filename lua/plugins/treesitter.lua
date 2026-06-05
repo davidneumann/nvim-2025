0bf04b0132fef1a21ce5a95702f5a427ff6f3ac4
David Neumann <davidjneumann@gmail.com>
2026-06-05 02:19:20 -0400

    Update treesitter to support nvim 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,41 +1,40 @@
-return
--- {
--- 	{
--- 		"nvim-treesitter/nvim-treesitter",
--- 		build = ":TSUpdate",
--- 		config = function()
--- 			local configs = require("nvim-treesitter.configs")
---
--- 			vim.g.skip_ts_context_comment_string_module = true
--- 			configs.setup({
--- 				sync_install = false,
--- 				ignore_install = {},
--- 				-- Installs are done by home-manager
--- 				ensure_installed = {},
--- 				-- ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
--- 				-- Autoinstall languages that are not installed
--- 				auto_install = false,
--- 				highlight = {
--- 					enable = true,
--- 					-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
--- 					--  If you are experiencing weird indenting issues, add the language to
--- 					--  the list of additional_vim_regex_highlighting and disabled languages for indent.
--- 					additional_vim_regex_highlighting = { "ruby" },
--- 				},
--- 				-- indent = { enable = true, disable = { 'ruby' } },
--- 				indent = { enable = true, disable = { "ruby", "typescript", "typescritreact" } },
--- 			})
--- 		end,
--- 	},
--- 	{
--- 		"nvim-treesitter/nvim-treesitter-context",
--- 		opts = {
--- 			max_lines = 3,
--- 		},
--- 	},
--- }
-{
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate'
+return {
+	{
+		'nvim-treesitter/nvim-treesitter',
+		lazy = false,
+		build = ':TSUpdate',
+		config = function()
+			-- Enable treesitter highlighting for common languages
+			vim.api.nvim_create_autocmd('FileType', {
+				pattern = {
+					'go', 'lua', 'vim', 'vimdoc', 'bash', 'c', 'cpp',
+					'javascript', 'typescript', 'typescriptreact', 'jsx',
+					'python', 'rust', 'html', 'css', 'json', 'yaml',
+					'markdown', 'markdown_inline', 'toml', 'sql',
+					'query', 'diff', 'regex', 'proto', 'dockerfile',
+				},
+				callback = function()
+					vim.treesitter.start()
+				end,
+			})
+
+			-- Enable treesitter-based indentation
+			vim.api.nvim_create_autocmd('FileType', {
+				pattern = {
+					'go', 'lua', 'c', 'cpp', 'javascript', 'typescript',
+					'typescriptreact', 'python', 'rust', 'html', 'css',
+					'json', 'yaml', 'toml', 'proto', 'dockerfile',
+				},
+				callback = function()
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
+	},
+	{
+		'nvim-treesitter/nvim-treesitter-context',
+		opts = {
+			max_lines = 3,
+		},
+	},
 }
